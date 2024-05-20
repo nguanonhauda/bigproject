14087cc15c0aa49f9b112835242c61926b7c3be3
Nguyễn Duy Minh <160730305+nguanonhauda@users.noreply.github.com>
2024-05-20 09:20:26 +0700

    Update App.java

--- src/Code/Logn in/App.java
+++ src/Code/Logn in/App.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import java.io.IOException;
 
@@ -35,41 +34,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-=======
-import java.io.IOException;
 
-import javafx.application.Application;
-
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-
-import javafx.stage.Stage;
- 
-public class App extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        
-  
-        Parent root;
-        try {
-        root = FXMLLoader.load(getClass().getResource("/Login/login.fxml"));
-        Scene scene = new Scene(root);
-  
-        primaryStage.setTitle("Phần Mềm Quản Lí Bán Hàng");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        } 
-        
-        catch (IOException e) {
-   
-        e.printStackTrace();
-        }
-    }
- 
-    public static void main(String[] args) {
-        launch(args);
-    }
->>>>>>> 87b736cf36ca43b85959edd3674d9c63a3f52d67
 }
